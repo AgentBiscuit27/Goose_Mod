bdcd1e2a3e73461ffbcb3ed97dfb70de7bb4b513
AgentBiscuit27 <amacdonald502@gmail.com>
2023-08-08 10:20:49 +0200

    Added items

--- src/main/java/net/duckieman/goosemod/GooseMod.java
+++ src/main/java/net/duckieman/goosemod/GooseMod.java
@@ -1,6 +1,9 @@
 package net.duckieman.goosemod;
 
 import com.mojang.logging.LogUtils;
+import net.duckieman.goosemod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,7 +14,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(GooseMod.MOD_ID)
@@ -22,6 +29,8 @@ public class GooseMod {
 
     public GooseMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -36,7 +45,15 @@ public class GooseMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            List<Item> items = ModItems.ITEMS.getEntries()
+                    .stream()
+                    .map(RegistryObject::get)
+                    .collect(Collectors.toList());
+            for (Item item : items) {
+                event.accept(item);
+            }
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/duckieman/goosemod/item/ModItems.java
@@ -0,0 +1,81 @@
+package net.duckieman.goosemod.item;
+
+import net.duckieman.goosemod.GooseMod;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, GooseMod.MOD_ID);
+
+    public static final RegistryObject<Item> PIZZA = ITEMS.register("pizza",
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(10).saturationMod(14).build())));
+
+    public static final RegistryObject<Item> DOUGH = ITEMS.register("dough",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TOMATO = ITEMS.register("tomato",
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(2).saturationMod(1).build())));
+
+    public static final RegistryObject<Item> TOMATO_SEEDS = ITEMS.register("tomato_seeds",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHEESE = ITEMS.register("cheese",
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(3).nutrition(2).build())));
+
+    public static final RegistryObject<Item> PEPPERONI = ITEMS.register("pepperoni",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SAUSAGE = ITEMS.register("sausage",
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(4).saturationMod(2).build())));
+
+    public static final RegistryObject<Item> BASIL = ITEMS.register("basil",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> EXTRAORDINARY_SOUP = ITEMS.register("extraordinary_soup",
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(16).saturationMod(14).build())));
+
+    public static final RegistryObject<Item> SOUP_POT = ITEMS.register("soup_pot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHICKEN_BONE = ITEMS.register("chicken_bone",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ONION = ITEMS.register("onion",
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(1).saturationMod(1).build())));
+
+    public static final RegistryObject<Item> ONION_SEEDS = ITEMS.register("onion_seeds",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SALT_AND_PEPPER = ITEMS.register("salt_and_pepper",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> NITKA_DO_ZUPY = ITEMS.register("nitka_do_zupy",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_DUCK_MEAT = ITEMS.register("raw_duck_meat",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_GOOSE_MEAT = ITEMS.register("raw_goose_meat",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GOOSE_TOES = ITEMS.register("goose_toes",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DUCK_TOES = ITEMS.register("duck_toes",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GOOSE_SPAWN_EGG = ITEMS.register("goose_spawn_egg",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DUCK_SPAWN_EGG = ITEMS.register("duck_spawn_egg",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
