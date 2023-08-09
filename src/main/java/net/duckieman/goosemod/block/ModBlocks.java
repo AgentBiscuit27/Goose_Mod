e00d9ad5e89d3848a3178eab5ca1a192015e8348
AgentBiscuit27 <amacdonald502@gmail.com>
2023-08-09 17:38:59 +0200

    Added rotation to pizza oven

--- src/main/java/net/duckieman/goosemod/block/ModBlocks.java
+++ src/main/java/net/duckieman/goosemod/block/ModBlocks.java
@@ -1,11 +1,11 @@
 package net.duckieman.goosemod.block;
 
 import net.duckieman.goosemod.GooseMod;
+import net.duckieman.goosemod.block.custom.HorizontalRotationBlock;
 import net.duckieman.goosemod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
@@ -21,7 +21,7 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, GooseMod.MOD_ID);
 
     public static final RegistryObject<Block> PIZZA_OVEN = registerBlock("pizza_oven",
-    () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(50.0F, 1200.0F)));
+    () -> new HorizontalRotationBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(50.0F, 1200.0F)) {});
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
