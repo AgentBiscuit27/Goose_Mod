5dca694ea684108c5ae4fd79d3670f7a2c66717c
AgentBiscuit27 <amacdonald502@gmail.com>
2023-08-13 22:34:27 +0200

    Added custom tags

--- src/main/java/net/duckieman/goosemod/item/custom/FuelItem.java
+++ src/main/java/net/duckieman/goosemod/item/custom/FuelItem.java
@@ -1,9 +1,14 @@
 package net.duckieman.goosemod.item.custom;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class FuelItem extends Item {
     private int burnTime = 0;
@@ -16,5 +21,10 @@ public class FuelItem extends Item {
     @Override
     public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
         return this.burnTime;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 }
